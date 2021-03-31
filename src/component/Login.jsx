0fb2c50dce453992c58f7e3d4af4b37b386de057
jcheahh <jackcheah2@gmail.com>
2021-03-31 22:44:30 +0800

    Finish Login page UI

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     Link,
     useHistory,
@@ -8,28 +8,18 @@ import { Form, Field } from "react-final-form";
 import { useAuth } from "./useAuth";
 
 export default function Login() {
+    const [user, setUser] = useState();
     const history = useHistory();
     const location = useLocation();
     const auth = useAuth();
 
     const { from } = location.state || { from: { pathname: "/" } };
-    const login = () => {
-        auth.login(() => {
-            history.replace(from);
-        });
-    };
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-
-        // console.log("hellllllo");
-    };
 
     const validate = (values) => {
         const error = {};
         if (!values.email) {
             error.email = "Fill in your email";
-        } else if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(values.email)) {
+        } else if (!/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(values.email)) {
             // Check email
             error.email = "Please enter a vaild email address";
         }
@@ -39,43 +29,101 @@ export default function Login() {
         }
         return error;
     };
-    return (
-        <>
-            <Link to="/">Home</Link>
-            <h1>Log in</h1>
-            <Form
-                onSubmit={onSubmit}
-                validate={validate}
-                render={({ handleSubmit }) => (
-                    <form onSubmit={handleSubmit}>
-                        <p>Email Address</p>
-                        <Field
-                            name="email"
-                            render={({ input, meta }) => (
-                                <input
-                                    required
-                                    id="email"
-                                    name="email"
-                                    autoComplete="email"
-                                    error={!!meta.error && meta.touched}
-                                    helperText={meta.touched ? meta.error : undefined}
-                                    {...input}
-                                />
-                            )}
-                        />
-                        <p>Password</p>
-                        <input type="password" required />
-                        <br />
-                        <p>
-                            Don&apos;t have an account? Sign up
-                            {" "}
-                            <Link to="/sign-up">here</Link>
-                        </p>
-                        <button type="button" onClick={login}>Log in</button>
-                    </form>
-                )}
-            />
 
-        </>
+    const handleChange = (e) => {
+        setUser(e.target.values);
+    };
+
+    function handleSubmit(e) {
+        // e.preventDefault();
+
+        auth.login(() => {
+            history.replace(from);
+        });
+    }
+
+    return (
+        <div className="mx-auto mt-36 lg:w-1/2 xl:max-w-screen-sm">
+            <div className="text-2xl text-indigo-800 tracking-wide ml-2 font-semibold">
+                <Link to="/">Home</Link>
+            </div>
+            <div className="mt-10 px-16 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:px-24 xl:max-w-2xl">
+                <h1 className="mt-24 text-center text-4xl text-indigo-900 font-display font-semibold lg:text-left xl:text-5xl
+                    xl:text-bold"
+                >
+                    Log in
+
+                </h1>
+                <Form
+                    onSubmit={handleSubmit}
+                    validate={validate}
+                    render={({ handleSubmit, submitting }) => (
+                        <div className="mt-12">
+                            <form onSubmit={handleSubmit} noValidate>
+                                <div className="text-sm font-bold text-gray-700 tracking-wide">Email</div>
+                                <Field
+                                    name="email"
+                                    render={({ input, meta }) => (
+                                        <>
+                                            <input
+                                                className={`w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500 ${meta.touched && !!meta.error ? "border-red-500" : ""}`}
+                                                placeholder="jack@gmail.com"
+                                                id="email"
+                                                name="email"
+                                                autoComplete="email"
+                                                onChange={handleChange}
+                                                {...input}
+                                            />
+                                            {meta.touched && !!meta.error && <p className="text-red-500 text-xs italic text-sm">{meta.error}</p>}
+                                        </>
+                                    )}
+                                />
+                                <div className="mt-8">
+                                    <div className="text-sm font-bold text-gray-700 tracking-wide">Password</div>
+                                    <Field
+                                        name="password"
+                                        render={({ input, meta }) => (
+                                            <>
+                                                <input
+                                                    className={`w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500 ${meta.touched && !!meta.error ? "border-red-500" : ""}`}
+                                                    placeholder="Enter your password"
+                                                    id="password"
+                                                    name="password"
+                                                    type="password"
+                                                    autoComplete="password"
+                                                    onChange={handleChange}
+                                                    {...input}
+                                                />
+                                                {meta.touched && !!meta.error && <p className="text-red-500 text-xs italic text-sm">{meta.error}</p>}
+                                            </>
+                                        )}
+                                    />
+                                </div>
+                                <br />
+                                <div className="flex mt-0.5 text-sm font-display font-semibold text-gray-700">
+                                    Don&apos;t have an account?&nbsp;
+                                    {" "}
+                                    <p className="flex-initial cursor-pointer text-indigo-600 hover:text-indigo-800">
+                                        <Link to="/sign-up">Sign up</Link>
+                                    </p>
+                                </div>
+                                <button
+                                    className="bg-indigo-500 text-gray-100 p-4 w-full rounded-full tracking-wide
+                                font-semibold font-display focus:outline-none focus:shadow-outline hover:bg-indigo-600
+                                shadow-lg mt-4"
+                                    type="submit"
+                                    disabled={submitting}
+                                // onClick={login}
+                                >
+                                    Log in
+
+                                </button>
+                            </form>
+                        </div>
+                    )}
+                />
+            </div>
+
+        </div>
     );
 }
