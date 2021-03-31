fdc8516d6f9bfe00f24657a35e52e850c76d99de
jcheahh <jackcheah2@gmail.com>
2021-03-31 22:43:35 +0800

    Change some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,11 +22,14 @@ module.exports = {
         indent: ["error", 4],
         "react/jsx-indent": ["error", 4],
         "react/jsx-indent-props": ["error", 4],
-        "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+        // "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
         camelcase: "off",
         "no-shadow": "off",
         "react/jsx-props-no-spreading": "off",
         "react/destructuring-assignment": "off",
         "import/prefer-default-export": "off",
+
+        // TODO: remove this
+        "no-unused-vars": "off",
     },
 };
