0e12ea4ae847e714f40a0caa19a1ab77858d8ca0
jcheahh <jackcheah2@gmail.com>
2021-04-01 15:24:37 +0800

    Add tailwind on landing page

--- src/component/LandingPage.jsx
+++ src/component/LandingPage.jsx
@@ -5,10 +5,16 @@ import {
 
 export default function LandingPage() {
     return (
-        <>
-            <h1>Welcome</h1>
-            <Link to="/login">Log In</Link>
-            <Link to="/sign-up">Sign Up</Link>
-        </>
+        <div className="App shadow font-sans px-9 py-16 flex justify-start flex-col w-4/6 max-w-screen-sm bg-white mx-auto my-28 rounded-2xl">
+            <h1 className="font-bold text-7xl text-gray-800 text-center mt-24">Welcome</h1>
+            <div className="text-center flex space-x-4 mt-16">
+                <div className="flex-1 text-2xl bg-purple-100 focus:outline-none text-sm py-2.5 px-8 rounded-2xl font-bold hover:bg-purple-600 hover:text-purple-100 text-purple-700 transform self-center">
+                    <Link to="/login">Log In</Link>
+                </div>
+                <div className="flex-1 text-2xl bg-purple-100 focus:outline-none text-sm py-2.5 px-8 rounded-2xl font-bold hover:bg-purple-600 hover:text-purple-100 text-purple-700 transform self-center">
+                    <Link to="/sign-up">Sign Up</Link>
+                </div>
+            </div>
+        </div>
     );
 }
