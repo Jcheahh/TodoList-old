c2bb105b16cacb95e5d253f497cab8f7c2da07b8
jcheahh <jackcheah2@gmail.com>
2021-04-02 21:29:48 +0800

    Move useProvideAuth to useAuth page and add css for logout button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import {
     BrowserRouter as Router,
@@ -10,7 +10,7 @@ import TodoList from "./component/TodoList";
 import Login from "./component/Login";
 import SignUp from "./component/SignUp";
 import LandingPage from "./component/LandingPage";
-import { authContext, useAuth } from "./component/useAuth";
+import { authContext, useAuth, useProvideAuth } from "./component/useAuth";
 
 function App() {
     return (
@@ -35,47 +35,6 @@ function App() {
     );
 }
 
-const fakeAuth = {
-    isAuthenticated: false,
-    login(cb) {
-        fakeAuth.isAuthenticated = true;
-        setTimeout(cb, 100); // fake async
-    },
-    signup(cb) {
-        fakeAuth.isAuthenticated = true;
-        setTimeout(cb, 100); // fake async
-    },
-    signout(cb) {
-        fakeAuth.isAuthenticated = false;
-        setTimeout(cb, 100);
-    },
-};
-
-function useProvideAuth() {
-    const [user, setUser] = useState(null);
-
-    const login = (cb) => fakeAuth.login(() => {
-        setUser("user");
-        cb();
-    });
-    const signup = (cb) => fakeAuth.signup(() => {
-        setUser("user");
-        cb();
-    });
-
-    const signout = (cb) => fakeAuth.signout(() => {
-        setUser(null);
-        cb();
-    });
-
-    return {
-        user,
-        login,
-        signup,
-        signout,
-    };
-}
-
 function ProvideAuth({ children }) {
     const auth = useProvideAuth();
     return (
@@ -84,55 +43,6 @@ function ProvideAuth({ children }) {
         </authContext.Provider>
     );
 }
-
-// function PrivateRoute({ children, ...rest }) {
-//   const auth = useAuth();
-
-//   return (
-//     <Route
-//       {...rest}
-//       render={({ location, history }) =>
-//         auth.user ? (
-//           <>
-//             <button onClick={() => auth.signout(() => {
-//               history.replace("/login");
-//             })}>Logout</button>
-//             {children}
-//           </>
-//         ) : (
-//           <Redirect
-//             to={{
-//               pathname: "/login",
-//               state: { from: location }
-//             }}
-//           />
-//         )
-//       }
-//     />
-//   );
-// }
-
-// function HomeRoute({ children, ...rest }) {
-//   const auth = useAuth();
-
-//   return (
-//     <Route
-//       {...rest}
-//       render={({ location, history }) =>
-//         auth.user ? (
-//           <>
-//             <button onClick={() => auth.signout(() => {
-//               history.replace("/login");
-//             })}>Logout</button>
-//             {children}
-//           </>
-//         ) : (
-//           <LandingPage />
-//         )
-//       }
-//     />
-//   );
-// }
 
 function CheckLoggedIn(renderComponent) {
     return function Component({ children, ...rest }) {
@@ -146,6 +56,7 @@ function CheckLoggedIn(renderComponent) {
                     <>
                         <button
                             type="button"
+                            className="absolute right-20 top-10 font-bold text-gray-700 hover:text-black"
                             onClick={() => auth.signout(() => {
                                 routerProps.history.replace("/login");
                             })}
