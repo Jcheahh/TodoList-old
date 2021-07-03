9a9912b8b95ce8472ae9645af77d725984321057
jcheahh <jackcheah2@gmail.com>
2021-07-03 22:59:49 +0800

    Add new color

--- src/component/ui/color.js
+++ src/component/ui/color.js
@@ -1,10 +1,11 @@
 export const main1 = "#FF7F50"; // Coral
-// export const main2 = "indigo-500"; // Lighter purple
-export const gray1 = "gray-700";
-export const gray2 = "gray-600";
+export const gray1 = "gray-900";
+export const gray2 = "gray-700";
 export const gray3 = "gray-300";
 export const gray4 = "gray-200";
 export const gray5 = "gray-100";
+export const red1 = "red-400";
+export const red2 = "red-600";
 
 export function toText(color) {
     return `text-${color}`;
