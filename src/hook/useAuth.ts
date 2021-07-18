545a9b0040270e6e04a86cfae3c5e31bd181b530
jcheahh <jackcheah2@gmail.com>
2021-07-18 15:37:48 +0800

    Update useAuth with types, remove cb and errorCb use Promise

--- src/hook/useAuth.ts
+++ src/hook/useAuth.ts
@@ -1,58 +1,80 @@
+import { AxiosError } from "axios";
 import { useContext, createContext, useState } from "react";
 import http from "../http";
 
-export const authContext = createContext();
+interface Auth {
+  user: string | null;
+  login: (email: string, password: string) => Promise<void>;
+  signup: (
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    password_confirmation: string,
+  ) => Promise<void>;
+  signout: () => Promise<void>;
+}
+
+export const authContext = createContext<Auth | null>(null);
 
 export function useAuth() {
-    return useContext(authContext);
+  return useContext(authContext);
 }
 
 export function useProvideAuth() {
-    const [user, setUser] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState(localStorage.getItem("token"));
 
-    const login = (email, password, cb, errorCb) => {
-        http.post("/api/users/sign_in", {
-            user: {
-                email,
-                password,
-            },
-        }).then((response) => {
-            setUser(response.data.token);
-            localStorage.setItem("token", response.data.token);
-            cb();
-        }).catch((error) => {
-            errorCb(error.response);
-        });
-    };
-    const signup = (firstName, lastName, email, password, password_confirmation, cb, errorCb) => {
-        http.post("/api/users", {
-            user: {
-                first_name: firstName,
-                last_name: lastName,
-                email,
-                password,
-                password_confirmation,
-            },
+  const login = (email: string, password: string): Promise<void> =>
+    http
+      .post("/api/users/sign_in", {
+        user: {
+          email,
+          password,
+        },
+      })
+      .then((response) => {
+        setUser(response.data.token);
+        localStorage.setItem("token", response.data.token);
+      })
+      .catch((error: AxiosError) => {
+        throw error.response;
+      });
+  const signup = (
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    password_confirmation: string,
+  ): Promise<void> =>
+    http
+      .post("/api/users", {
+        user: {
+          first_name: firstName,
+          last_name: lastName,
+          email,
+          password,
+          password_confirmation,
+        },
+      })
+      .then((response) => {
+        setUser(response.data.token);
+        localStorage.setItem("token", response.data.token);
+      })
+      .catch((error: AxiosError) => {
+        throw error.response;
+      });
 
-        }).then((response) => {
-            setUser(response.data.token);
-            localStorage.setItem("token", response.data.token);
-            cb();
-        }).catch((error) => {
-            errorCb(error.response);
-        });
-    };
+  const signout = (): Promise<void> =>
+    new Promise((resolve, _reject) => {
+      setUser(null);
+      localStorage.removeItem("token");
+      resolve();
+    });
 
-    const signout = (cb) => {
-        setUser(null);
-        localStorage.removeItem("token");
-        cb();
-    };
-
-    return {
-        user,
-        login,
-        signup,
-        signout,
-    };
+  return {
+    user,
+    login,
+    signup,
+    signout,
+  };
 }
