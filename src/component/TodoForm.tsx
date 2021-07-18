f50ab9d6e27e0320ab7d72393a2ec47eac2d82de
jcheahh <jackcheah2@gmail.com>
2021-07-18 15:34:01 +0800

    Update TodoForm with types

--- src/component/TodoForm.tsx
+++ src/component/TodoForm.tsx
@@ -1,68 +1,73 @@
-import React, { useState, useEffect, useRef } from "react";
-import PropTypes from "prop-types";
+import React, { useState, useLayoutEffect, useRef, ChangeEvent } from "react";
 
-function TodoForm(props) {
-    const [todo, setTodo] = useState(props.edit ? props.edit.value : "");
+function TodoForm(props: {
+  defaultValue?: string;
+  onSubmit: (todo: string) => void;
+}): JSX.Element {
+  const [todo, setTodo] = useState(props.defaultValue || "");
 
-    function handleChange(e) {
-        setTodo(e.target.value);
-    }
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setTodo(e.target.value);
+  }
 
-    const inputRef = useRef(null);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-    useEffect(() => {
-        inputRef.current.focus();
-    });
+  useLayoutEffect(() => {
+    inputRef.current!.focus();
+  });
 
-    function handleSubmit(e) {
-        e.preventDefault();
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
 
-        props.onSubmit(todo);
+    props.onSubmit(todo);
 
-        setTodo("");
-    }
+    setTodo("");
+  }
 
-    return (
-        <form className="flex" onSubmit={handleSubmit}>
-            {props.edit ? (
-                <>
-                    <input
-                        placeholder="Update your item"
-                        value={todo}
-                        onChange={handleChange}
-                        name="text"
-                        ref={inputRef}
-                        autoComplete="off"
-                        className="bg-gray-100 border-gray-100 flex-1 focus-within:border-current focus:outline-none focus:ring-2 focus:ring-purple-400 placeholder-gray-600 px-6 py-6 my-6 mr-1.5 h-10 rounded-2xl text-gray-900"
-                    />
-                    <button type="button" onClick={handleSubmit} className="bg-purple-100 focus:outline-none text-sm py-2.5 px-8 rounded-2xl font-bold hover:bg-purple-600 hover:text-purple-100 text-purple-700 transform self-center text-lg">
-                        Save
-                    </button>
-                </>
-            ) : (
-                <>
-                    <input
-                        name="task"
-                        className="bg-gray-100 border-gray-100 flex-1 focus-within:border-current focus:outline-none focus:ring-2 focus:ring-purple-400 placeholder-gray-600 px-6 py-6 my-6 mr-1.5 h-10 rounded-2xl text-gray-900"
-                        type="text"
-                        value={todo}
-                        placeholder="Add something"
-                        onChange={handleChange}
-                        ref={inputRef}
-                        autoComplete="off"
-                    />
-                    <button type="button" onClick={handleSubmit} className="bg-purple-100 focus:outline-none text-sm py-2.5 px-8 rounded-2xl font-bold hover:bg-purple-600 hover:text-purple-100 text-purple-700 transform self-center text-lg">
-                        Add
-                    </button>
-                </>
-            )}
-        </form>
-    );
+  return (
+    <form className="flex" onSubmit={handleSubmit}>
+      {props.defaultValue ? (
+        <>
+          <input
+            placeholder="Update your item"
+            value={todo}
+            onChange={handleChange}
+            name="text"
+            ref={inputRef}
+            autoComplete="off"
+            className="bg-gray-100 border-gray-100 flex-1 focus-within:border-current focus:outline-none focus:ring-2 focus:ring-purple-400 placeholder-gray-600 px-6 py-6 my-6 mr-1.5 h-10 rounded-2xl text-gray-900"
+          />
+          <button
+            type="button"
+            onClick={handleSubmit}
+            className="bg-purple-100 focus:outline-none text-sm py-2.5 px-8 rounded-2xl font-bold hover:bg-purple-600 hover:text-purple-100 text-purple-700 transform self-center text-lg"
+          >
+            Save
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            name="task"
+            className="bg-gray-100 border-gray-100 flex-1 focus-within:border-current focus:outline-none focus:ring-2 focus:ring-purple-400 placeholder-gray-600 px-6 py-6 my-6 mr-1.5 h-10 rounded-2xl text-gray-900"
+            type="text"
+            value={todo}
+            placeholder="Add something"
+            onChange={handleChange}
+            ref={inputRef}
+            autoComplete="off"
+          />
+          <button
+            type="button"
+            onClick={handleSubmit}
+            className="bg-purple-100 focus:outline-none text-sm py-2.5 px-8 rounded-2xl font-bold hover:bg-purple-600 hover:text-purple-100 text-purple-700 transform self-center text-lg"
+          >
+            Add
+          </button>
+        </>
+      )}
+    </form>
+  );
 }
-
-TodoForm.propTypes = {
-    edit: PropTypes.string.isRequired,
-    onSubmit: PropTypes.func.isRequired,
-};
 
 export default TodoForm;
