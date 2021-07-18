a91217336a9f9f922dbeb15b87a204136ff5f470
jcheahh <jackcheah2@gmail.com>
2021-07-18 15:56:48 +0800

    Update LandingPage and http indentation and change go to href

--- src/component/LandingPage.tsx
+++ src/component/LandingPage.tsx
@@ -1,37 +1,36 @@
 import React from "react";
-import {
-    Link,
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Button } from "./ui/Button";
 import NavBar from "./ui/NavBar";
 import { Text } from "./ui/Text";
 import { Link as Link1 } from "./ui/Link";
 
 export default function LandingPage(): JSX.Element {
-    return (
-        <>
-            <NavBar>
-                <Link1 className={["mr-4 font-semibold"]} to="/login">Log In</Link1>
-                <Link to="/sign-up">
-                    <Button type="button">Sign Up</Button>
-                </Link>
-            </NavBar>
-            <div className="flex justify-center">
-
-                <div className="text-left max-w-2xl py-14">
-                    <Text.H1 className={["font-bold"]}>
-                        <span className="text-6xl text-gray-700 block">Todo List,</span>
-                        <span>Reimagined</span>
-                    </Text.H1>
-                    <p className="py-3 text-2xl">
-                        Todolist lets you quickly add and organize tasks,
-                        including recurring tasks and mark as done when you completed
-                    </p>
-                    <Button isLarge type="button">
-                        Demo
-                    </Button>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <NavBar>
+        <Link1 className={["mr-4 font-semibold"]} to="/login">
+          Log In
+        </Link1>
+        <Link to="/sign-up">
+          <Button type="button">Sign Up</Button>
+        </Link>
+      </NavBar>
+      <div className="flex justify-center">
+        <div className="text-left max-w-2xl py-14">
+          <Text.H1 className={["font-bold"]}>
+            <span className="text-6xl text-gray-700 block">Todo List,</span>
+            <span>Reimagined</span>
+          </Text.H1>
+          <p className="py-3 text-2xl">
+            Todolist lets you quickly add and organize tasks, including
+            recurring tasks and mark as done when you completed
+          </p>
+          <Button isLarge type="button">
+            Demo
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/http.ts
+++ src/http.ts
@@ -1,32 +1,32 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:4567",
+  baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:4567",
 });
 
 http.interceptors.request.use(
-    async (config) => {
-        const token = localStorage.getItem("token");
-        if (token) {
-            // eslint-disable-next-line no-param-reassign
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error),
+  async (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      // eslint-disable-next-line no-param-reassign
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
 );
 
 http.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        const { status } = error.response || {};
-        if (status === 401) {
-            localStorage.removeItem("token");
+  (response) => response,
+  (error) => {
+    const { status } = error.response || {};
+    if (status === 401) {
+      localStorage.removeItem("token");
 
-            window.history.go("/login");
-        }
-        return Promise.reject(error);
-    },
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  },
 );
 
 export default http;
